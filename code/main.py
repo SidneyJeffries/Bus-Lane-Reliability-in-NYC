4ca8ffbde12df393822877ae05130a5322192088
SidneyJeffries <sidney.r.jeffries@gmail.com>
2026-09-22 19:20:05 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import glob
 
-files = glob.glob('Comparison Periods/GTFS-RT data/Feb/Week 1/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/Feb/Week 1/*.parquet')
 
 feb1_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 feb1_rt_data.head()
@@ -12,14 +12,14 @@ feb1_rt_data.columns
 
 #February Week 2
 
-files = glob.glob('Comparison Periods/GTFS-RT data/Feb/Week 2/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/Feb/Week 2/*.parquet')
 
 feb2_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 feb2_rt_data.head()
 feb2_rt_data.columns
 
 #April Week 1
-files = glob.glob('Comparison Periods/GTFS-RT data/April/Week 1/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/April/Week 1/*.parquet')
 
 apr1_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 apr1_rt_data.head()
@@ -27,7 +27,7 @@ apr1_rt_data.columns
 
 #April Week 2
 
-files = glob.glob('Comparison Periods/GTFS-RT data/April/Week 2/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/April/Week 2/*.parquet')
 
 apr2_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 apr2_rt_data.head()
@@ -35,7 +35,7 @@ apr2_rt_data.columns
 
 #July Week 1
 
-files = glob.glob('Comparison Periods/GTFS-RT data/July/Week 1/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/July/Week 1/*.parquet')
 
 jul1_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 jul1_rt_data.head()
@@ -43,7 +43,7 @@ jul1_rt_data.columns
 
 #July Week 2
 
-files = glob.glob('Comparison Periods/GTFS-RT data/July/Week 2/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/July/Week 2/*.parquet')
 
 jul2_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 jul2_rt_data.head()
@@ -51,7 +51,7 @@ jul2_rt_data.columns
 
 #October Week 1
 
-files = glob.glob('Comparison Periods/GTFS-RT data/October/Week 1/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/October/Week 1/*.parquet')
 
 oct1_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 oct1_rt_data.head()
@@ -60,7 +60,7 @@ oct1_rt_data.columns
 #October Week 2
 
 
-files = glob.glob('Comparison Periods/GTFS-RT data/October/Week 2/*.parquet')
+files = glob.glob('data/Comparison Periods/GTFS-RT data/October/Week 2/*.parquet')
 
 oct2_rt_data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
 oct2_rt_data.head()
